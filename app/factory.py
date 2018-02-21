40efed20261261276ec6bcec0a353335095cf0f5
fajardm <fajardm@wonderlabs.io>
2018-02-21 18:09:10 +0700

    add setup file and factory

---
+++ app/factory.py
@@ -0,0 +1,30 @@
+from app.news.news_controller import module_news
+from flask import Flask, jsonify
+from flask_migrate import Migrate
+from helpers.database import db
+from helpers.marshmallow import ma
+
+
+def create_app(config=None):
+    app = Flask('app')
+
+    app.config.update(config or {})
+    app.config.from_pyfile('config.cfg', silent=True)
+
+    db.init_app(app)
+    Migrate(app, db)
+    ma.init_app(app)
+
+    register_blueprints(app)
+
+    @app.route('/ping')
+    def get_ping():
+        res = jsonify(status='success', data={'message': 'pong'})
+        return res
+
+    return app
+
+
+def register_blueprints(app):
+    app.register_blueprint(module_news, url_prefix='/news')
+    return app
--- run.py
+++ run.py
@@ -1,29 +1,6 @@
-from app.news.news_controller import module_news
-from flask import Flask, jsonify
-from flask_migrate import Migrate
-from helpers.database import db
-from helpers.marshmallow import ma
+from app.factory import create_app
 
-# Define the WSGI application object
-app = Flask(__name__)
-
-# Configurations
-app.config.from_pyfile('config.cfg', silent=True)
-
-# Initialize database
-db.init_app(app)
-migrate = Migrate(app, db)
-
-ma.init_app(app)
-
-app.register_blueprint(module_news, url_prefix='/news')
-
-
-@app.route('/ping')
-def get_ping():
-    res = jsonify(status='success', data={'message': 'pong'})
-    return res
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3000, debug=True)
