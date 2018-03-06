53dca0342151c409326af7679d2c719d0bbcfaec
fajardm <fajardm@wonderlabs.io>
2018-03-06 15:57:33 +0700

    refactor dockerized

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-sleep 30s
+echo "Waiting for postgres..."
+
+while ! nc -z example_news_api_db 3306; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
 
 export FLASK_APP=run.py && flask db init && flask db migrate && flask db upgrade
 
